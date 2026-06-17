f7ed331824497048919bee6b9b949b28cda3fe67
chile07-hub <chilevasudev@gmail.com>
2026-06-17 11:01:40 +0000

    class commit

---
+++ menu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char option;
+    printf("<-----Welcome to chile's Restaurent----->\n\n");
+    printf("Pleases order\n");
+    printf("1.Starter\n2.Main Course\n");
+}
